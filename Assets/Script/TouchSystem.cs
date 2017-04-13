6b889ef3314d56289e6511e9ca95421a4980951c
DaisukeNakamura <jacco.dnacol@gmail.com>
2017-04-13 20:34:17 +0900

    mausu

---
+++ Assets/Script/TouchSystem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TouchSystem : MonoBehaviour {
+	private const int MOUSE_CLICK_LEFT = 0;
+	private const int MOUSE_CLICK_RIGHT = 1;
+	private const int MOUSE_CLICK_WHEEL = 2;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButton(MOUSE_CLICK_LEFT) == true)
+		{
+			Debug.Log("Mouse：" + Input.mousePosition);
+		}
+	}
+}
