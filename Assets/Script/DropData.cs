b220bf4666b14c69342212e511f2cf2ed2848d76
DaisukeNakamura <jacco.dnacol@gmail.com>
2017-05-02 18:37:23 +0900

    ソース整理
    ・未使用の変数削除
    ・コメント修正

--- Assets/Script/DropData.cs
+++ Assets/Script/DropData.cs
@@ -4,18 +4,15 @@ using System.Collections;
 public class DropData : MonoBehaviour {
 	private int text_number = 0;
 	private Vector2 pos = Vector2.zero;
-	private Vector2 size;
 	private int obj_id = 0;
 
 	private bool is_create = false;
 	// Use this for initialization
 	void Start () {
 
-		// サイズは可変することはない
+		// サイズは可変することはないので生成時に固定で設定してしまう
 		float sizeX = GetComponent<SpriteRenderer>().bounds.size.x;
 		float sizeY = GetComponent<SpriteRenderer>().bounds.size.y;
-		size.x = GetComponent<SpriteRenderer>().bounds.size.x;
-		size.y = GetComponent<SpriteRenderer>().bounds.size.y;
 		float scelaX = 1.0f / sizeX;
 		float scelaY = 1.0f / sizeY;
 		//transform.localScale = new Vector2(DropDataManager.DROP_SIZE, DropDataManager.DROP_SIZE);
@@ -55,21 +52,29 @@ public class DropData : MonoBehaviour {
 			Debug.Log("不正なIDを設定しようとしています");
 			return;
 		}
-		text_number = in_num;
 
+		// 表示する番号の設定
+		// 見た目の情報であり、内部的なID番号ではない
+		text_number = in_num;
+		setSprite(text_number);
+
+		// ID番号の設定
+		// 内部的な情報であり、見た目の番号ではない
 		obj_id = in_pos_id;
 
 		// 生成するオブジェクトの名前を設定
 		// (ここで設定しないとすべてマテリアル名(Clone)になる)
 		this.name = "DropMaterial" + obj_id;
 
-		// 3D座標に変換
-		Vector2 vec;
-		vec.x = (in_pos_id % DropDataManager.DROP_WIDTH_MAX);
-		vec.y = (in_pos_id / DropDataManager.DROP_HEIGHT_MAX);
-		SetPos(vec);
+		// ポジションの設定
+		{
+			// 3D座標に変換
+			Vector2 vec;
+			vec.x = (in_pos_id % DropDataManager.DROP_WIDTH_MAX);
+			vec.y = (in_pos_id / DropDataManager.DROP_HEIGHT_MAX);
+			SetPos(vec);
+		}
 
-		setSprite(obj_id);
 		is_create = true;
 	}
 
@@ -86,8 +91,8 @@ public class DropData : MonoBehaviour {
 	/// <summary>
 	/// 指定したIDに応じたスプライトを設定する
 	/// </summary>
-	/// <param name="in_pos_id"></param>
-	public void setSprite(int in_pos_id)
+	/// <param name="in_text_number"></param>
+	public void setSprite(int in_text_number)
 	{
 		Sprite image = Resources.Load<Sprite>("Image/TestSprite");
 		SpriteRenderer sprite_renderer = GetComponent<SpriteRenderer>();
